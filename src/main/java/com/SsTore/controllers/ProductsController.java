e503b402b48a2d40b9310afa89444364f6d7a17e
abdelilah dehaoui <aboudahaoui123456789@gmail.com>
2020-06-09 21:18:18 +0100

    prd.pkg upload file problem

--- src/main/java/com/SsTore/controllers/ProductsController.java
+++ src/main/java/com/SsTore/controllers/ProductsController.java
@@ -12,10 +12,12 @@ import com.SsTore.domains.Product.Product;
 import com.SsTore.services.IFileService;
 import com.springBootLibrary.controllers.BaseCrudController;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.util.concurrent.ExecutionException;
@@ -26,6 +28,18 @@ public class ProductsController extends BaseCrudController<Product, ProductDto, 
     @Autowired
     private IFileService iFileService;
 
+    @PostMapping(value = "/createWithImages", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ProductDto createWithImages(
+            @RequestPart(value = "images") MultipartFile[] images,
+            @RequestPart(value = "product") @Valid ProductCreateDto productCreateDto) throws ExecutionException, InterruptedException {
+
+        for (var img : productCreateDto.getImages()) {
+            iFileService.UploadFile(img);
+        }
+
+        return super.create(productCreateDto);
+    }
+   /*
     @PostMapping("/createWithImages")
     public ProductDto createWithImages(@Valid @ModelAttribute ProductCreateDto productCreateDto) throws ExecutionException, InterruptedException {
         for (var img : productCreateDto.getImages()) {
@@ -33,6 +47,6 @@ public class ProductsController extends BaseCrudController<Product, ProductDto, 
         }
 
         return super.create(productCreateDto);
-    }
+    }*/
 }
 
